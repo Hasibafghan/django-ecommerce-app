6d5f4fc6545af3832cef4337442368d46ce02db6
Hasib Alamyar <hasibalamyar2@gmail.com>
2025-05-22 03:16:49 +0430

    files strutuerd

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,11 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
 
+setTimeout(function () {
+    var container = document.getElementById('messages-container')
+    if (container) {
+        container.style.transition = 'opacity 0.7s'
+        container.style.opacity = '0'
+        setTimeout(function () {
+            container.style.display = 'none'
+        }, 700)
+    }
+}, 2500)
