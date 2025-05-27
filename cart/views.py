49db200a8164828c8bc9f9e8894a5fe9daa9399b
Hasib Alamyar <hasibalamyar2@gmail.com>
2025-05-27 23:28:08 +0430

    view_cart , remove_cart added

--- cart/views.py
+++ cart/views.py
@@ -8,10 +8,13 @@ def view_cart(request):
 
     for item_id, quantity in cart.items():
         product = get_object_or_404(Product, pk=item_id)
-        total = product.price * quantity
+        if product.is_in_discount:
+            unit_price = product.discount
+        else:
+            unit_price = product.price
+        total = unit_price * quantity
         total_price += total
-        cart_items.append({'product': product, 'quantity': quantity, 'total_price': total})
-
+        cart_items.append({'product': product, 'quantity': quantity, 'total_price': total, 'unit_price': unit_price})
     return render(request, 'cart_view.html', {'cart_items': cart_items, 'total_price': total_price})
 
 def add_to_cart(request):
@@ -22,7 +25,7 @@ def add_to_cart(request):
 
         if item_id in cart:
             cart[item_id] += quantity
-        else:
+        else:    
             cart[item_id] = quantity
 
         request.session['cart'] = cart
@@ -31,7 +34,11 @@ def add_to_cart(request):
         return render(request, 'add_to_cart.html')
 
 def remove_from_cart(request, item_id):
-    pass
+    cart = request.session.get('cart', {})
+    if item_id in cart:
+        del cart[item_id]
+        request.session['cart'] = cart
+    return redirect('view_cart')
 
 def update_cart_item(request, item_id):
     pass
